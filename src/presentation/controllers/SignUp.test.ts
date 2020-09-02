b4c081ced29a3e651efe010c2ef8b2d98b568315
Glauber Brack <glauber@brack.com.br>
2020-09-02 13:19:15 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/SignUp.test.ts
+++ src/presentation/controllers/SignUp.test.ts
@@ -43,4 +43,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissignParamError('password'))
   })
+
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Glauber Brack',
+        email: 'glauber@brack.com.br',
+        password: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissignParamError('passwordConfirmation'))
+  })
 })
